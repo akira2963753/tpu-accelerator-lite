a99faf5041778b275bd042a64ae70b98eac9d38d
akira2963753 <harry2963753@gmail.com>
2026-08-01 23:51:22 +0800

    Consolidate synthesis setup

--- src/02_SYN/syn16.tcl
+++ src/02_SYN/syn16.tcl
@@ -3,9 +3,52 @@
 #=================================================================
 
 #=================================================================
+#-------------------- Set 16 nm Library Path ---------------------
+#=================================================================
+set search_path    "/usr/cad/ADFP/Executable_Package/Collaterals/IP/stdcell/N16ADFP_StdCell/CCS/ \
+                    /usr/cad/ADFP/Executable_Package/Collaterals/IP/stdio/N16ADFP_StdIO/NLDM/ \
+                    /usr/cad/ADFP/Executable_Package/Collaterals/IP/sram/N16ADFP_SRAM/NLDM/ \
+                    $search_path .\
+                    "
+
+set target_library "N16ADFP_StdCellff0p88v125c_ccs.db \
+                    N16ADFP_StdCellff0p88vm40c_ccs.db \
+                    N16ADFP_StdCellss0p72v125c_ccs.db \
+                    N16ADFP_StdCellss0p72vm40c_ccs.db \
+                    N16ADFP_StdCelltt0p8v25c_ccs.db \
+                    N16ADFP_StdIOff0p88v1p98v125c.db \
+                    N16ADFP_StdIOff0p88v1p98vm40c.db \
+                    N16ADFP_StdIOss0p72v1p62v125c.db \
+                    N16ADFP_StdIOss0p72v1p62vm40c.db \
+                    N16ADFP_StdIOtt0p8v1p8v25c.db \
+                    N16ADFP_SRAM_ff0p88v0p88v125c_100a.db \
+                    N16ADFP_SRAM_ff0p88v0p88vm40c_100a.db \
+                    N16ADFP_SRAM_ss0p72v0p72v125c_100a.db \
+                    N16ADFP_SRAM_ss0p72v0p72vm40c_100a.db \
+                    N16ADFP_SRAM_tt0p8v0p8v25c_100a.db \
+                    "
+
+set link_library "* $target_library dw_foundation.sldb"
+set symbol_library "generic.sdb"
+set synthetic_library "dw_foundation.sldb"
+
+#=================================================================
+#---------- Global Setting and Environment Optimization ----------
+#=================================================================
+set hdlin_auto_save_templates true
+set hdlin_check_no_latch true
+set verilogout_no_tri true
+set sh_enable_line_editing true
+history keep 1000
+alias h history
+set sh_continue_on_error false
+set compile_preserve_subdesign_interfaces true
+
+#=================================================================
 #--------------------- TOP Module Definition ---------------------
 #=================================================================
 set DESIGN  "TPU"
+set CYCLE 2
 
 #=================================================================
 #------------- Create the Working and Saving Folders -------------
@@ -28,15 +71,32 @@ current_design $DESIGN
 set_operating_conditions -max_library N16ADFP_StdCellss0p72vm40c_ccs -max ss0p72vm40c \
     -min_library N16ADFP_StdCellff0p88vm125c_ccs -min ff0p88vm125c
 
-source constraint.sdc
+#=================================================================
+#------------------------- Create Clock --------------------------
+#=================================================================
+create_clock -name clk -period $CYCLE [get_ports clk]
+set_dont_touch [all_clocks]
+set_ideal_network [all_clocks]
+# set_fix_hold [all_clocks]
+set_cost_priority -min_delay
+set_clock_uncertainty -setup 0.5 -hold 0.005 [all_clocks]
+set_clock_latency 0.5 [all_clocks]
+set_clock_latency -source 0 [all_clocks]
+set_clock_transition 0.2 [all_clocks]
+
+#=================================================================
+#---------------------- Timing Constraints -----------------------
+#=================================================================
+set_input_delay [expr $CYCLE * 0.5] -clock clk [all_inputs]
+set_output_delay [expr $CYCLE * 0.5] -clock clk [all_outputs]
+set_input_transition 0.5 [all_inputs]
+
 #=================================================================
 #-------------------- Design Rule Constraints --------------------
 #=================================================================
 
 set_driving_cell -library "N16ADFP_StdCellss0p72vm40c_ccs" -lib_cell BUFFD4BWP16P90LVT -pin {Z} [get_ports clk]
 set_driving_cell -library "N16ADFP_StdCellss0p72vm40c_ccs" -lib_cell DFQD1BWP16P90LVT -pin {Q} [remove_from_collection [all_inputs] [get_ports clk]]
-
-
 set_load [load_of "N16ADFP_StdCellss0p72vm40c_ccs/DFQD1BWP16P90LVT/D"] [all_outputs]
 
 set_max_area 0
